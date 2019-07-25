7df575026dc835b3c574a11fba27b21a127bda7e
Hamza Aburaneh <32944558+HamzaAburaneh@users.noreply.github.com>
2019-07-25 10:49:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
 /*
-NOTE: THIS CODE WILL NOT RUN UNTIL YOU ENTER YOUR OWN openweathermap.org APP ID KEY
-
 NOTE: You need to intall the npm modules by executing >npm install
 before running this server
-
-Simple express server re-serving data from openweathermap.org
-To test: 
-http://localhost:3000
-or
-http://localhost:3000/weather?recipe=Ottawa
-to just set JSON response. (Note it is helpful to add a JSON formatter extension, like JSON Formatter, to your Chrome browser for viewing just JSON data.)
 */
 const express = require('express') //express framework
 const requestModule = require('request') //npm module for easy http requests
 const PORT = process.env.PORT || 3000
 
-/*YOU NEED AN APP ID KEY TO RUN THIS CODE
-  GET ONE BY SIGNING UP AT openweathermap.org
-  THE KEY BELOW IS FAKE
-*/
+// YOU NEED AN APP ID KEY TO RUN THIS CODE
+
 const FOOD2FORK_API_KEY = '08b4ef60dec92dadac663d11375b3458' //PUT IN YOUR OWN KEY HERE
 
 const app = express()
